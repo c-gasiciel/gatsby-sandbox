db58d3699c870b8a1f781885cca0dd1ca1175663
c-gasiciel <clarissa.gasiciel@gmail.com>
2021-09-07 20:47:39 -0500

    Added header help section and styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,30 +2,33 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 import { 
     layoutContainer,
-    heading,
+    pageHeader,
+    headerContact,
     navContainer,
     navLogo,
     navLinks,
 } from '../styles/layout.module.css'
 import NavLink from './navLink'
 
-const Layout = ({ pageTitle, children }) => {
+const Layout = ({ pageTitle, children}) => {
     return (
         <div className={layoutContainer}>
             <title>{pageTitle}</title>
-            <nav className={navContainer}>
-                <h1>
-                    <Link className={navLogo} to="/">My Technology Firm</Link>
-                </h1>
-                <ul className={navLinks}>
-                    <NavLink linkURL="/" linkName="Home" />
-                    <NavLink linkURL="/services" linkName="Services" />
-                    <NavLink linkURL="/about" linkName="About Us" />
-                    <NavLink linkURL="/contact" linkName="Contact Us" />
-                </ul>
-            </nav>
+            <header className={ pageHeader }>
+                <p className={headerContact} >Have a question or need a quote? (XXX)XXX-XXXX</p>
+                <nav className={ navContainer }>
+                    <h1>
+                        <Link className={navLogo} to="/">My Technology Firm</Link>
+                    </h1>
+                    <ul className={navLinks}>
+                        <NavLink linkURL="/" linkName="Home" />
+                        <NavLink linkURL="/services" linkName="Services" />
+                        <NavLink linkURL="/about" linkName="About Us" />
+                        <NavLink linkURL="/contact" linkName="Contact Us" />
+                    </ul>
+                </nav>
+            </header>
             <main>
-                <h1 className={heading}>{pageTitle}</h1>
                 {children}
             </main>
         </div>
