2abeeb697eccc169ce4f57529244afcbab393b9b
c-gasiciel <clarissa.gasiciel@gmail.com>
2021-09-07 21:23:41 -0500

    Added spaces for readability

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,16 +13,16 @@ import NavLink from './navLink'
 
 const Layout = ({ pageTitle, children}) => {
     return (
-        <div className={layoutContainer}>
-            <title>{pageTitle}</title>
+        <div className={ layoutContainer }>
+            <title>{ pageTitle }</title>
             <header className={ pageHeader }>
                 <h1>
-                    <Link className={navLogo} to="/">My Technology Firm</Link>
+                    <Link className={ navLogo } to="/">My Technology Firm</Link>
                 </h1>
-                <div className={rightHeader}>
-                    <p className={headerContact} >Have a question or need a quote? (XXX)XXX-XXXX</p>
+                <div className={ rightHeader }>
+                    <p className={ headerContact }>Have a question or need a quote? (XXX)XXX-XXXX</p>
                     <nav className={ navContainer }>
-                        <ul className={navLinks}>
+                        <ul className={ navLinks }>
                             <NavLink linkURL="/" linkName="Home" />
                             <NavLink linkURL="/services" linkName="Services" />
                             <NavLink linkURL="/about" linkName="About Us" />
